23b4323882c873639afb174f57efc1c27882529b
Andres Alejo <aalejog09@gmail.com>
2024-10-15 14:01:19 -0400

    update: role sql bck

--- doc/db_backup/master_tables/_role__20241005_bck-1728108911599.sql
+++ doc/db_backup/master_tables/_role__20241005_bck-1728108911599.sql
@@ -1,2 +1,3 @@
-INSERT INTO public."role" (creation_date,"name") VALUES
-	 ('2024-10-05 02:08:54.469415','ADMIN');
+
+	INSERT INTO public."role" (id,creation_date,"name","enabled") VALUES
+	 (1,'2024-10-05 02:08:54.469415','ADMIN',1);
